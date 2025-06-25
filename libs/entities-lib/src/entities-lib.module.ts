ab3683fb036bf5c092f49174a150e42d3ba18352
Jadtales <amjad.massaoud@innowise.com>
2025-06-25 10:41:14 +0200

    refactor: remove unnecessary library

--- libs/entities-lib/src/entities-lib.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EntitiesLibService } from './entities-lib.service';
-
-@Module({
-  providers: [EntitiesLibService],
-  exports: [EntitiesLibService],
-})
-export class EntitiesLibModule {}
--- libs/entities-lib/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './entities-lib.module';
-export * from './entities-lib.service';
-export * from './entities/user.entity';
